16d94648910f22fdc77a7289f44e984c51f40304
AdexeMonzon <adexems.projects@gmail.com>
2025-05-14 19:39:28 +0100

    Upload basic program

---
+++ src/main/java/com/libros_autores_api/libros_autores_api/LibrosAutoresApiApplication.java
@@ -0,0 +1,13 @@
+package com.libros_autores_api.libros_autores_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosAutoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosAutoresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libros_autores_api/libros_autores_api/LibrosAutoresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libros_autores_api.libros_autores_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosAutoresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
